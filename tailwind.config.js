f02515f74815f18aa4429d3c63ba626e6ff3a58e
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-06-27 15:20:46 +0330

    git add .

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./build/*.html",
+    "./build/pages/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
