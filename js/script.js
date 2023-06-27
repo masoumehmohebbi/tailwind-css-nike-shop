3b4042172e4191ccf08c8a34414ef376d0056d81
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-06-27 19:04:22 +0330

    Updates

--- js/script.js
+++ js/script.js
@@ -9,3 +9,17 @@ document.querySelector('#menu').addEventListener('click', (e) => {
     document.querySelector('.sidebar').classList.toggle('change-sidebar');
     document.querySelector('#hamburger-icon').classList.toggle('change-hamburger-icon');
 });
+
+// Mobile Footer
+const bottonNav = document.querySelector('#bottom-navigation');
+
+    for (let i = 0; i < bottonNav.children.length; i++) {
+        bottonNav.children[i].addEventListener('click', function (e) {
+            for (const tab of bottonNav.children) {
+              tab.children[0].children[1].style.display = 'none';
+              tab.children[0].children[0].classList.add('text-gray-400');
+        }
+        this.children[0].children[1].style.display = 'block';
+        this.children[0].children[0].classList.remove('text-gray-400');
+    });
+}
