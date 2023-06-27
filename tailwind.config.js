774f8901c37a9b19a160da954255dbd82b5af136
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-06-27 17:01:55 +0330

    Added: Custom Color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,21 @@ module.exports = {
     "./build/pages/*.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["vazir"],
+      },
+      screens:{
+        '-xs':  {'max': '486px'},
+        '-sm':  {'max': '640px'},
+        '-md':  {'max': '768px'},
+        '+md':  {'min': '860px'},
+      },
+      colors: {
+        'primary':'#3ba3df',
+        'secondary':'#e832b5'
+      }
+    },
   },
   plugins: [],
 }
